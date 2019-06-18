bf83e73b9a662dc11cd6d9160b289188223a4574
Dr.Voloshin <eralashpvp@gmail.com>
2019-06-18 14:17:22 +0300

    some work

---
+++ scripts/lang.js
@@ -0,0 +1,7 @@
+var ul = document.getElementById('toogle');
+ul.addEventListener('click',func);
+function func(){
+    document.getElementById('trainer').innerHTML='11111';
+    document.getElementById('trainer2').innerHTML='u and me u and me'
+   
+}
