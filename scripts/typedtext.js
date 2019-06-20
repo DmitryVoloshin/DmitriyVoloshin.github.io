b0309a26541b35a47f3897161ec360cc61e6f854
Dr.Voloshin <eralashpvp@gmail.com>
2019-06-20 15:54:46 +0300

    +jQuery + Script(tiped) + bibl(typed.min.js)

---
+++ scripts/typedtext.js
@@ -0,0 +1,16 @@
+
+        let typed = new Typed(".type", {
+            strings: [
+                "jsut for test", 
+                "some text for test"
+              ],
+            typeSpeed: 60,
+            backSpeed: 90,
+          });
+          
+          
+          
+           
+          
+          
+              
