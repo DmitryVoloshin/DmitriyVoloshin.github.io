ff7e2b4cd686b6b79191907a5412a9f76ba28a4c
Dr.Voloshin <eralashpvp@gmail.com>
2019-06-21 17:26:59 +0300

    UPD for hamburger JS prob its work

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -10,6 +10,20 @@ $(document).click(()=>{
 //
 $(document).on('click','.hamburger-pulldown_toogle',(event)=>{
 
+    let el = $(event.currentTarget);
 
-    
-})
+    event.preventDefault();
+    event.stopPropagation();
+
+
+    if(el.hasClass('open')){
+        el.removeClass('open');
+    }else{
+        if($lastOpened){
+            $lastOpened.removeClass('open')
+        }
+        el.addClass('open');
+        $lastOpened = el
+    }
+
+});
